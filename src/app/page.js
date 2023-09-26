1255636f23c14261f7ea4530700181a9d203e2e2
Diego <diegobqu@ucm.es>
2023-09-26 14:20:50 +0200

    Shadcn UI Integration

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 'use client'
 import MainMap from '@components/MainMap'
+import { Button } from '@components/ui/ui/button'
 import styles from './page.module.css'
 
 export default function Home () {
   return (
-    <MainMap />
+    <>
+      <Button onClick={() => console.log('A')}>Click me</Button>
+      <MainMap />
+    </>
   )
 }
